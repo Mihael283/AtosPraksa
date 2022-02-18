c0b1995c137c07b2a16a1010693b03d5bec356da
Mihael Španović <span.mihael@gmail.com>
2022-02-18 03:12:15 -0800

    Initial push cmake

---
+++ CMakePriprema/zadatak2/src/Professor.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+#include </home/mikeee/Documents/zadatak2/include/Professor/Professor.h>
+using namespace std;
+
+Professor::Professor(string name){
+    professorName = name;
+}
---
+++ CMakePriprema/zadatak2/src/Student.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include </home/mikeee/Documents/zadatak2/include/Student/Student.h>
+using namespace std;
+
+
+Student::Student(string name){
+    studentName = name;
+}
+
