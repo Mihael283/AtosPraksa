26abf9e103938bbe5c4c80ffd2ecb98563baf4f3
Mihael Španović <span.mihael@gmail.com>
2022-02-22 04:54:08 -0800

    Zadatak 7 Cmake remake

--- CMakePriprema/zadatak7/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mihael/Documents/GitHub/AtosPraksa/CMakePriprema/zadatak7
-# Build directory: /home/mihael/Documents/GitHub/AtosPraksa/CMakePriprema/zadatak7/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
