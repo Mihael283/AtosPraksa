5e8dd0058a653956919c5bdc3188a79c7c44e07c
Mihael Španović <span.mihael@gmail.com>
2022-02-24 11:58:51 +0100

    Created empty branch

--- CMakePriprema/zadatak2/src/Professor.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cstring>
-#include </home/mikeee/Documents/zadatak2/include/Professor/Professor.h>
-using namespace std;
-
-Professor::Professor(string name){
-    professorName = name;
-}
--- JavaPriprema/zadatak13/Language.java
+++
@@ -1,6 +0,0 @@
-package zadatak13;
-
-public interface Language {
-	void getName(String name); // interface method (does not have a body)
-	void getType(String type);
-}
